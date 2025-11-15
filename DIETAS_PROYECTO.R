1cd9acf4814dc1b728c4db8abe0a9d6c8d90b164
josurb123 <rezabadajosue@gmail.com>
2025-11-15 14:20:13 -0600

    sacar cantidad de macromoleculas
    en esta vez agregue 2 data.frames (frutas yverduras), puse otro readline para saber si la persona quería subir de peso, después saque las calorías ajustadas a si la persona queria subir de peso, bajar o mantenerse. Finalmente saque la cantidad (en gramos) de proteínas, carbohidratos y lípidos que debe consumir la persona

--- DIETAS_PROYECTO.R
+++ DIETAS_PROYECTO.R
@@ -36,6 +36,32 @@ lacteos_df <- data.frame(
   alergenos = c("lacteos", "lacteos", "lacteos", "lacteos")
 )
 
+frutas_df <- data.frame(
+  alimento = c("manzana", "platano", "naranja", "fresa", "uva", "melon", 
+               "sandia", "piña", "mango", "kiwi", "pera", "durazno",
+               "ciruela", "cereza", "arandanos", "frambuesa", "zarzamora"),
+  calorias_por_100g = c(52, 89, 47, 32, 69, 34, 30, 50, 60, 61, 57, 39, 
+                        46, 50, 57, 52, 43),
+  carbohidratos_por_100g = c(14, 23, 12, 8, 18, 8, 8, 13, 15, 15, 15, 10,
+                             11, 12, 14, 12, 10),
+  alergenos = c("ninguno", "ninguno", "ninguno", "ninguno", "ninguno", "ninguno",
+                "ninguno", "ninguno", "ninguno", "ninguno", "ninguno", "ninguno",
+                "ninguno", "ninguno", "ninguno", "ninguno", "ninguno")
+)
+
+verduras_df <- data.frame(
+  alimento = c("brocoli", "espinaca", "zanahoria", "tomate", "pepino", 
+               "lechuga", "coliflor", "pimiento", "calabaza", "berenjena", 
+               "apio", "champinones", "alcachofa","esparrago", "col_bruselas"),
+  calorias_por_100g = c(34, 23, 41, 18, 15, 15, 25, 26, 26, 25, 
+                        16, 22, 47, 20, 43),
+  carbohidratos_por_100g = c(7, 4, 10, 4, 3, 3, 5, 6, 7, 6, 
+                             3, 3, 11, 4, 9),
+  proteinas_por_100g = c(2.8, 2.9, 0.9, 0.9, 0.7, 1.4, 2.0, 1.0, 1.0, 1.0,
+                         0.7, 3.1, 3.3, 2.2, 3.4),
+  alergenos = c("ninguno", "ninguno", "ninguno", "ninguno", "ninguno", "ninguno",
+                "ninguno", "ninguno", "ninguno", "ninguno", "ninguno", "ninguno", "ninguno", "ninguno", "ninguno")
+)
 #lo valores nutrimentales (aproximaciones) se obtuvieron de busquedas a través de internet, estos valores pueden cambiar según la pagina web de donde saques los datos
 
 #Tiene que calcular: calorias y cantidad de proteina
@@ -52,7 +78,7 @@ dieta <- function(){
                        ejercicio fuerte (6-7 días), ejercicio muy fuerte (2 veces por dia)")
 alergias<- readline(prompt = "Tiene alergia a alguno de estos grupos alimentarios? (lacteos, gluten, frutos secos). 
                       Si usted tiene alergia a alguno de estos grupos, coloquelo en minusculas (gluten) de lo contrario, coloque (ninguna)")
-  
+cambio_peso<-readline(prompt="Quieres subir de peso, bajar de peso, o mantenerlo? (contesta con minusculas, escogiendo estas 3 opciones: subir, bajar, mantener)")
   #CALCULAR LAS CALORIAS QUE DEBE COMER
   ##hacemos la formula para sacar las calorias que debes de consumir según el peso, la altura, la edad y la actividad fisica
   #los valores obtenido de esta fórmula se le denomina "tasa metabolica basal"
@@ -68,8 +94,20 @@ alergias<- readline(prompt = "Tiene alergia a alguno de estos grupos alimentario
   }else if(sexo=="femenino" & actividad== "ejercicio fuerte"){mujeres_TMB<- (10*peso)+(6.25*altura)-(5*edad)-161; calorias<-mujeres_TMB*1.725
   }else if(sexo=="femenino" & actividad== "ejercicio muy fuerte"){mujeres_TMB<- (10*peso)+(6.25*altura)-(5*edad)-161; calorias<-mujeres_TMB*1.9
   }else{print("corriga su respuesta")
-    }
-  ###Después de eso hay que sacar las proporciones de carbohidratos, proteinas y lipidos que debe tener su dieta
+  }
+
+##Después de esto hay que sacar las calorías que debe consumir la persona ajustandolo a si quiere bajar o subir de peso
+##valores de multiplicación obtenidos de la página web "fit generation"
+if(cambio_peso=="bajar"){calorias_ajustado<-calorias*0.8
+}else if(cambio_peso=="subir"){calorias_ajustado<-calorias*1.15
+}else{calorias_ajustado<-calorias
+}
+###Después de eso hay que sacar las proporciones de carbohidratos, proteinas y lipidos que debe tener su dieta
+#las cantidades se sacan orque a partir de las calorias totales aproximadamente 50% deben provenir de carbohidratos, 30% de lípidos, 20% de proteínas
+#luego se dividen para sacar la cantidad en gramos
+cantidad_carbohidratos<- (calorias_ajustado*0.5)/4
+cantidad_proteínas<- calorias_ajustado*0.2/4
+cantidad_lípidos<-calorias_ajustado*0.3/9
 
 
   
